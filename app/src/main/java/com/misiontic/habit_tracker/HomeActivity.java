e1ad5d48032ecf18c500aeba6120403e3efa9011
ratara5 <ratara5@gmail.com>
2021-11-08 21:48:32 -0500

    Agregando elementos a activity_home (En proceso)

--- app/src/main/java/com/misiontic/habit_tracker/HomeActivity.java
+++ app/src/main/java/com/misiontic/habit_tracker/HomeActivity.java
@@ -1,21 +1,28 @@
 package com.misiontic.habit_tracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.TextView;
+
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
 
     private TextView tvEmail,tvProveedor;
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        mToolbar=findViewById(R.id.topAppBar);
+        setSupportActionBar(mToolbar);
 
         tvEmail=(TextView)findViewById(R.id.emailTextView);
         tvProveedor=(TextView)findViewById(R.id.providerTextView);
@@ -24,6 +31,14 @@ public class HomeActivity extends AppCompatActivity {
         tvEmail.setText(llave); //user[0] o user.email?
         tvProveedor.setText(llave); //user[1] o user.provider?
 
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
     }
 
     public void exit(View view) { //No existe esta referencia en OnClic
