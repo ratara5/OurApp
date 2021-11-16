9d465b3ffd6ecf1bd57346f09b933ee7a0dc9055
victor <vialma678@gmail.com>
2021-11-15 20:32:10 -0500

    Conexión ToolsFragment con HabitsActivity, pendiente mejorar vista xml

--- app/src/main/java/com/misiontic/habit_tracker/HabitsActivity.java
+++ app/src/main/java/com/misiontic/habit_tracker/HabitsActivity.java
@@ -3,6 +3,7 @@ package com.misiontic.habit_tracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class HabitsActivity extends AppCompatActivity {
 
@@ -11,4 +12,6 @@ public class HabitsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_habits);
     }
+
+
 }
--- app/src/main/java/com/misiontic/habit_tracker/ToolsFragment.java
+++ app/src/main/java/com/misiontic/habit_tracker/ToolsFragment.java
@@ -1,5 +1,7 @@
 package com.misiontic.habit_tracker;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +9,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +17,8 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class ToolsFragment extends Fragment {
+
+    Button btnPushUps;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -23,6 +28,7 @@ public class ToolsFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
 
     public ToolsFragment() {
         // Required empty public constructor
@@ -55,10 +61,22 @@ public class ToolsFragment extends Fragment {
         }
     }
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tools, container, false);
+        // return inflater.inflate(R.layout.fragment_tools, container, false);
+        View view = inflater.inflate(R.layout.fragment_tools,container,false);
+
+        btnPushUps = (Button) view.findViewById(R.id.btnPushUps);
+        btnPushUps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), HabitsActivity.class);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 }
