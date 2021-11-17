96f25deb0c2c5900cdcc6d13837eb3d7dd7c28ab
victor <vialma678@gmail.com>
2021-11-17 16:31:16 -0500

    Contador de flexiones funcional

--- app/src/main/java/com/misiontic/habit_tracker/HabitsActivity.java
+++ app/src/main/java/com/misiontic/habit_tracker/HabitsActivity.java
@@ -3,15 +3,42 @@ package com.misiontic.habit_tracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class HabitsActivity extends AppCompatActivity {
+
+    private TextView textContenedorNumero;
+    private Button btnTouchAction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_habits);
+
+        encontrarComponentesPorId();
+
+        btnTouchAction.setOnClickListener(new  View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int intContenedorNumero = Integer.parseInt(textContenedorNumero.getText().toString());
+                Log.d("parse", "" + intContenedorNumero);
+                intContenedorNumero++;
+
+                String stringNumeroFinal = String.valueOf(intContenedorNumero);
+
+                textContenedorNumero.setText(stringNumeroFinal);
+
+            }
+        });
     }
 
+    private void encontrarComponentesPorId(){
+         textContenedorNumero= findViewById(R.id.textCont);
+        btnTouchAction = findViewById(R.id.btnAction);
+
+    }
 
 }
