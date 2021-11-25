25acfe1206e8b1a4acbc7bd1779e0c20c17f46e8
IsaMaru98 <mimarualandaa@unal.edu.co>
2021-11-24 20:49:05 -0500

    Diseño botones ListActivity

--- app/src/main/java/com/misiontic/habit_tracker/ListActivity.java
+++ app/src/main/java/com/misiontic/habit_tracker/ListActivity.java
@@ -3,6 +3,8 @@ package com.misiontic.habit_tracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
 
 public class ListActivity extends AppCompatActivity {
 
@@ -10,6 +12,14 @@ public class ListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+    }
+
+
+    public void HabitPreferences(View view){
+
+        CheckBox cbWater = findViewById(R.id.CBhabito1Agua);
+
+
     }
 
    // extendedFab.setOnClickListener {
