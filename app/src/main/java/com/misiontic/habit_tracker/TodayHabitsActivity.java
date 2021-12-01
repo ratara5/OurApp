75832ca8d1b8ca4f6453e894d272a08d05394336
ratara5 <ratara5@gmail.com>
2021-12-01 18:53:36 -0500

    item check (hábito de hoy) es leído desde base de datos tabla dates (En proceso)

--- app/src/main/java/com/misiontic/habit_tracker/TodayHabitsActivity.java
+++ app/src/main/java/com/misiontic/habit_tracker/TodayHabitsActivity.java
@@ -16,13 +16,15 @@ import com.misiontic.habit_tracker.listviews.HabitListViewAdapter;
 import com.misiontic.habit_tracker.model.Habits;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class TodayHabitsActivity extends AppCompatActivity {
 
-    private static ArrayList<Habits> todayHabitsList;
+    //private static ArrayList<Habits> todayHabitsList;
+    private static ArrayList<String> todayHabitsList;
     private static ListView listView2;
     private MySQLiteHelper connectionBD;
     private static ArrayAdapter adapter2;
@@ -33,46 +35,47 @@ public class TodayHabitsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_today_habits);
 
         listView2=findViewById(R.id.listView2);
+        todayHabitsList=new ArrayList<>();
 
         //Obtener con serializable
         /*
         todayHabitsList=(ArrayList<Habits>)getIntent().getSerializableExtra("today",0);
         */
+        /* Obtener normal
         int idHabitToday= getIntent().getIntExtra("checkedHabitId",0);
-
+        */
+        /* Obtener con savedInstance
         if(savedInstanceState == null || !savedInstanceState.containsKey("key")) {
-            // Obtener con bd
 
-            connectionBD = new MySQLiteHelper(this);
-            String sentence = "SELECT * FROM habits WHERE _id = ?";
-            String[] params = new String[]{String.valueOf(idHabitToday)};
-            Cursor results = connectionBD.getData(sentence, params);
-
-            results.moveToFirst();
-            int id = results.getInt(0);
-            int nameId = results.getColumnIndex("name");
-            String nameHabit = results.getString(nameId);
-            String descriptionHabit = results.getString(2);
-            String categoryHabit = results.getString(3);
-
-            Habits newHabit = new Habits(nameHabit, descriptionHabit, categoryHabit);
-
-            todayHabitsList=new ArrayList<>();
-            //newHabit.setId(id);
-            todayHabitsList.add(newHabit);
-        }  else {
-            todayHabitsList = savedInstanceState.getParcelableArrayList("key");
+        }}  else {
+        todayHabitsList = savedInstanceState.getParcelableArrayList("key");
         }
-        adapter2 = new ArrayAdapter<Habits>(this, android.R.layout.simple_list_item_1, todayHabitsList);
+        */
+        // Obtener con bd
+
+        connectionBD = new MySQLiteHelper(this);
+        String sentence = "SELECT * FROM dates WHERE date = ?";
+        String now = Calendar.getInstance().getTime().toString();
+        String[] params = new String[]{String.valueOf(now)};
+        Cursor results = connectionBD.getData(sentence, params);
+
+        results.moveToFirst();
+        do {
+            int id = results.getInt(0);
+            int dateId = results.getColumnIndex("name");
+            String date = results.getString(dateId);
+            int id_habit = results.getInt(2);
+
+
+            todayHabitsList.add(date+" - "+String.valueOf(id_habit));
+
+        } while (results.moveToNext());
+
+        adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, todayHabitsList);
         //adapter2.notifyDataSetChanged();
         listView2.setAdapter(adapter2);
 
 
     }
 
-    @Override
-    protected void onSaveInstanceState(@NonNull Bundle outState) {
-        outState.putParcelableArrayList("key",todayHabitsList);
-        super.onSaveInstanceState(outState);
-    }
 }
