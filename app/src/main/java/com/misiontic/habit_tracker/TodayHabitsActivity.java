139e17bbc1cb16392da2aa605267db3de9023e65
ratara5 <ratara5@gmail.com>
2021-12-09 12:18:22 -0500

    Implementación Calendario (En proceso)

--- app/src/main/java/com/misiontic/habit_tracker/TodayHabitsActivity.java
+++ app/src/main/java/com/misiontic/habit_tracker/TodayHabitsActivity.java
@@ -5,16 +5,21 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.app.DatePickerDialog;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import com.misiontic.habit_tracker.db.MySQLiteHelper;
 import com.misiontic.habit_tracker.listviews.HabitListViewAdapter;
+import com.misiontic.habit_tracker.model.DatePickerFragment;
 import com.misiontic.habit_tracker.model.Habits;
 
 import java.text.SimpleDateFormat;
@@ -33,13 +38,16 @@ public class TodayHabitsActivity extends AppCompatActivity {
     private MySQLiteHelper connectionBD;
     private static ArrayAdapter adapter2;
 
+    //Calendar
+    private EditText etDate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_today_habits);
 
-        listView2=findViewById(R.id.listView2);
-        todayHabitsList=new ArrayList<>();
+        listView2 = findViewById(R.id.listView2);
+        todayHabitsList = new ArrayList<>();
 
         Toolbar myChildToolbar =
                 (Toolbar) findViewById(R.id.mh_toolbar);
@@ -78,10 +86,10 @@ public class TodayHabitsActivity extends AppCompatActivity {
             int id = results.getInt(0);
             int dateId = results.getColumnIndex("date");
             String date = results.getString(dateId);
-            String time=results.getString(2);
-            String name_habit=results.getString(3);
+            String time = results.getString(2);
+            String name_habit = results.getString(3);
 
-            todayHabitsList.add(String.valueOf(id)+" - "+time+" - "+String.valueOf(name_habit));
+            todayHabitsList.add(String.valueOf(id) + " - " + date + " - "+ time + " - " + String.valueOf(name_habit));
 
         } while (results.moveToNext());
         results.close();
@@ -90,7 +98,35 @@ public class TodayHabitsActivity extends AppCompatActivity {
         //adapter2.notifyDataSetChanged();
         listView2.setAdapter(adapter2);
 
+        //Calendar
+        etDate = findViewById(R.id.etDate);
 
+        etDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePickerDialog();
+                //break;
+            }
+        });
+    }
+
+    private void showDatePickerDialog() {
+        DatePickerFragment newFragment = DatePickerFragment.newInstance(new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                // +1 because January is zero
+                String dy;
+                if(day<10){
+                     dy="0"+String.valueOf(day);
+                }else{
+                     dy=String.valueOf(day);
+                }
+                String selectedDate = dy + "-" + (month+1) + "-" + year;
+                etDate.setText(selectedDate);
+            }
+        });
+
+        newFragment.show(getSupportFragmentManager(), "datePicker");
     }
 
 }
